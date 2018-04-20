cf623f86c0fc46161aa9ae4eda801f870ce03bec
t0biwan <tobi.klatt@web.de>
2018-04-20 15:01:47 +0200

    Added navbar, form and table that all render from a given model

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+<div id="app">
+
+  <nav role="navigation">
+    <ul>
+      <li v-for="model in models" v-on:click="setActiveModel(model)" v-bind:key="model.name">{{model.name}}</li>
+    </ul>
+  </nav>
+
+  <form>
+    <template v-for="(propertie, key) in presentedModel.properties" v-if="propertie.editable">
+      <label v-bind:for="key">{{key}}:</label>
+      <input v-bind:name="key" v-bind:type="getInputType(propertie.type)" v-model="v" />
+    </template>
+  </form>
+
+  <table>
+    <thead>
+      <tr><th v-for="(propertie, key) in presentedModel.properties">{{key}}</th></tr>
+    </thead>
+    <tbody>
+      <tr><td></td></tr>
+    </tbody>
+  </table>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+// todo: laden der models in andere datei auslagern
+import customerModel from '@/models/customer.json'
+import orderModel from '@/models/order.json'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+
+  data () {
+    return {
+      // todo: laden der models in andere datei auslagern, in config angeben welche geladen werden sollen
+      customerModel,
+      orderModel,
+      presentedModel: customerModel,
+      models: [customerModel, orderModel]
+    }
+  },
+
+  methods: {
+    getInputType (type) {
+      if (type === 'string') return 'text'
+      else if (type === 'number') return 'number'
+      else if (type === 'boolean') return 'checkbox'
+    },
+
+    setActiveModel (model) {
+      this.presentedModel = model
+    },
   }
 }
 </script>
